1f6d5d4efc4822d8e157bdea1c1250995cfac18c
JKZuk <jacek.zagawa@gmail.com>
2020-06-21 08:36:56 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+
+namespace Pixels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Pixels pix = new Pixels();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSaveIMG.Text = Properties.Settings.Default.savePath;
+
+            label4.Visible = false;
+            label5.Visible = false;
+            label3.Visible = false;
+
+            panel1.AutoScroll = true;
+            panel2.AutoScroll = true;
+
+            Pixels.PicBox1 = pictureBox1;
+            Pixels.PicBox2 = pictureBox2;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy || backgroundWorker2.IsBusy ||backgroundWorker3.IsBusy)
+                {
+                    MessageBox.Show("I'm Busy!\nBe patient!", "Important Note", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    OpenFileDialog open = new OpenFileDialog();
+                    Pixels.Open = open;
+                    Pixels.Open.Filter = "Image Files(*.bmp)|*.bmp";
+
+                    if (open.ShowDialog() == DialogResult.OK)
+                    {
+                        Bitmap img = new Bitmap(Pixels.Open.FileName);
+                        Bitmap bitmap = img.Clone(new Rectangle(0, 0, img.Width, img.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                        Pixels.Img = img;
+                        Pixels.Bitmap = bitmap;
+
+                        pictureBox1.Visible = false;
+                        pictureBox2.Visible = false;
+
+                        button2.Enabled = false;
+                        button3.Enabled = false;
+
+                        label8.Text = "Opening and Reworking Image..please wait...";
+
+                        backgroundWorker1.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker3.IsBusy || backgroundWorker2.IsBusy ||backgroundWorker1.IsBusy)
+            {
+                MessageBox.Show("I'm Busy!\nBe patient!", "Important Note", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                label8.Text = "Reworking Image..please wait...";
+                backgroundWorker3.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            pix.ReworkImage(txtPixels.Text, Color.Blue);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+
+            pictureBox1.Image = Pixels.Bitmap;
+            pictureBox2.Image = Pixels.Img;
+
+            var imageHeight = Pixels.Img.Height;
+            var imageWidth = Pixels.Img.Width;
+            label1.Text = "X = " + imageWidth.ToString() + " px";
+            label2.Text = " |  Y = " + imageHeight.ToString() + " px";
+
+            pictureBox1.Image = pix.Zoom(pictureBox1, Convert.ToDouble(txtZoom.Text));
+
+            label8.Text = "";
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker2.IsBusy || backgroundWorker1.IsBusy ||backgroundWorker3.IsBusy)
+            {
+                MessageBox.Show("I'm Busy!\nBe patient!", "Important Note", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                label8.Text = "Applying Changes..please wait...";
+                txtSaveIMG.ReadOnly = true;
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            pix.ReworkImage(txtPixels.Text, Color.White);
+            pix.SaveChanges(txtSaveIMG.Text);
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pictureBox1.Image = pix.Zoom(pictureBox1, 100.0);
+            label8.Text = "";
+            txtSaveIMG.ReadOnly = false;
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap img = new Bitmap(Pixels.Open.FileName);
+            Bitmap bitmap = img.Clone(new Rectangle(0, 0, img.Width, img.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Pixels.Img = img;
+            Pixels.Bitmap = bitmap;
+
+            pix.ReworkImage(txtPixels.Text, Color.Blue);
+        }
+
+        private void backgroundWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pictureBox1.Image = pix.Zoom(pictureBox1, 100.0);
+            label8.Text = "";
+        }
+
+        private void txtSaveIMG_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.savePath = txtSaveIMG.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ Pixels.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Pixels
+{
+    public class Pixels
+    {
+        static Bitmap img;
+
+        public static Bitmap Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+
+        static Bitmap bitmap;
+
+        public static Bitmap Bitmap
+        {
+            get { return bitmap; }
+            set { bitmap = value; }
+        }
+
+        static OpenFileDialog open;
+
+        public static OpenFileDialog Open
+        {
+            get { return open; }
+            set { open = value; }
+        }
+
+        static int progressY;
+
+        public static int Progress
+        {
+            get { return progressY; }
+            set { progressY = value; }
+        }
+
+        static int progressMaxValue;
+
+        public static int ProgressMaxValue
+        {
+            get { return progressMaxValue; }
+            set { progressMaxValue = value; }
+        }
+
+        static PictureBox picBox1;
+
+        public static PictureBox PicBox1
+        {
+            get { return picBox1; }
+            set { picBox1 = value; }
+        }
+
+        static PictureBox picBox2;
+
+        public static PictureBox PicBox2
+        {
+            get { return picBox2; }
+            set { picBox2 = value; }
+        }
+
+        public Bitmap Zoom(PictureBox pictureBox, double magnification)
+        {
+            Bitmap bmp = null;
+            
+            try
+            {
+                if (img.Width < 5000 && img.Height < 2000)
+                {
+                    double zoom = magnification / 100.0;
+                    bmp = new Bitmap(Pixels.Bitmap, Convert.ToInt32(pictureBox.Width * zoom), Convert.ToInt32(pictureBox.Height * zoom));
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                }
+                else
+                {
+                    double zoom = 100.0 / 100.0;
+                    bmp = new Bitmap(Pixels.Bitmap, Convert.ToInt32(pictureBox.Width * zoom), Convert.ToInt32(pictureBox.Height * zoom));
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return bmp;
+        }
+
+        public void SaveChanges(string savePath)
+        {
+            try
+            {
+                Bitmap bm = new Bitmap(bitmap);
+                bm.Save(savePath, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void ReworkImage(string gapInPixels, Color colorRGB)
+        {
+            int progressCount_i_Y = 0;
+            int progressCount_j_Y = 0;
+
+            int black = 0;
+            int white = 0;
+
+            try
+            {
+                //Y Axis
+                for (int i = 0; i < img.Width; i++)
+                {
+                    for (int j = 0; j < img.Height; j++)
+                    {
+                        Color pixel = img.GetPixel(i, j);
+
+                        if (pixel.Name != "ffffffff") //if is not white
+                        {
+                            black++;
+                            if (white > 0 && white <= Convert.ToInt32(gapInPixels))
+                            {
+                                bitmap.SetPixel(i, j, colorRGB);
+
+                                i = i - white - 1;
+                                Color pixel1 = img.GetPixel(i, j);
+                                if (pixel1.Name == "ff000000")
+                                {
+                                    j--;
+                                    Color pixel2 = img.GetPixel(i, j);
+                                    if (pixel2.Name == "ffffffff")
+                                    {
+                                        i = i + white + 1;
+                                        Color pixel3 = img.GetPixel(i, j);
+                                        if (pixel3.Name == "ff000000")
+                                        {
+                                            bitmap.SetPixel(i, j, colorRGB); //blue
+                                            j++;
+                                        }
+                                        else
+                                        {
+                                            bitmap.SetPixel(i, j, Color.White); //white
+                                            j++;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        i = i + white + 1;
+                                        j++;
+                                    }
+                                }
+                                else
+                                {
+                                    i = i + white + 1;
+                                }
+                            }
+
+                            white = 0;
+                        }
+
+                        if (pixel.Name != "ff000000")
+                        {
+                            black = 0;
+                            white++;
+                        }
+                        progressCount_j_Y = j;
+                    }
+                    progressMaxValue = img.Width + img.Height - 10;
+                    progressCount_i_Y = i;
+                }
+                progressY = progressCount_i_Y + progressCount_j_Y;
+
+                //X Axis
+                for (int j = 0; j < img.Height; j++)
+                {
+                    for (int i = 0; i < img.Width; i++)
+                    {
+                        Color pixel = img.GetPixel(i, j);
+
+                        if (pixel.Name != "ffffffff") //if is not white
+                        {
+                            black++;
+                            if (white > 0 && white <= Convert.ToInt32(gapInPixels))
+                            {
+                                bitmap.SetPixel(i, j, colorRGB);
+
+                                i = i - white - 1;
+                                Color pixel1 = img.GetPixel(i, j);
+                                if (pixel1.Name == "ff000000")
+                                {
+                                    j--;
+                                    Color pixel2 = img.GetPixel(i, j);
+                                    if (pixel2.Name == "ffffffff")
+                                    {
+                                        i = i + white + 1;
+                                        Color pixel3 = img.GetPixel(i, j);
+                                        if (pixel3.Name == "ff000000")
+                                        {
+                                            bitmap.SetPixel(i, j, colorRGB);  //blue
+                                            j++;
+                                        }
+                                        else
+                                        {
+                                            bitmap.SetPixel(i, j, Color.White); //white
+                                            j++;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        i = i + white + 1;
+                                        j++;
+                                    }
+                                }
+                                else
+                                {
+                                    i = i + white + 1;
+                                }
+                            }
+
+                            white = 0;
+                        }
+
+                        if (pixel.Name != "ff000000")
+                        {
+                            black = 0;
+                            white++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
